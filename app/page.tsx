67d00e3a8b961e42a68ffa6735af7b807872975a
auspidiam <auspidiam@gmail.com>
2025-12-26 21:40:53 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
     <main className="flex-1 py-12">
       <h1 className="text-4xl font-semibold tracking-tight">Auspidiam</h1>
 
-      {/* very close + clearly visible */}
-      <hr className="mt-1 border-0 border-t-2 border-black" />
+      {/* closer + thinner */}
+      <hr className="mt-0.5 border-0 border-t-[0.5px] border-black" />
     </main>
   );
 }
