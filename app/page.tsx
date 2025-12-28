99186eaeed82a5f688f62cf30129b1ca60d98afb
auspidiam <auspidiam@gmail.com>
2025-12-28 17:37:32 -0500

    Align hero rule with hero image

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col py-12">
-      <div className="flex items-end">
-        <Image
-          src="/AuspidiamCursive.png"
-          alt="Auspidiam"
-          width={420}
-          height={90}
-          priority
-          className="h-16 w-auto"
-        />
-      </div>
+      <div className="flex w-full flex-col items-center sm:items-start">
+        <div className="flex w-full justify-center sm:justify-start">
+          <Image
+            src="/AuspidiamCursive.png"
+            alt="Auspidiam"
+            width={420}
+            height={90}
+            priority
+            className="h-16 w-auto"
+          />
+        </div>
 
-      {/* header rule: 1px */}
-      <hr className="mt-[-8px] w-full border-t border-solid border-black border-t-[0.5px]" />
+        {/* header rule: 1px */}
+        <hr className="mt-2 w-full max-w-[420px] border-t border-solid border-black" />
+      </div>
 
       {/* body copy only */}
       <section className="relative mt-10 mb-10 flex-1 w-full min-h-[320px]">
